0e6329ac02750d5b711bedcc74092184b10fe066
Evan Brecht-Curry <starfoxz13@gmail.com>
2020-02-19 16:20:47 -0800

    finished in o(n)

--- 1. Maximum Subarray/index.js
+++ 1. Maximum Subarray/index.js
@@ -1,15 +1,17 @@
 const max_sub_array_of_size_k = function(k, arr) {
-  let largestSoFar = 0;
-  for (let i = 0; i + k < arr.length + 1; i++) {
-    let sum = 0;
-    for (let j = i; j - i < k; j++) {
-      sum += arr[j];
-    }
-    if(sum > largestSoFar) {
-      largestSoFar = sum;
+  let maxSum = 0,
+    windowSum = 0,
+    windowStart = 0;
+  for (let i = 0; i < arr.length; i++) {
+    windowSum += arr[i];
+    if (i >= k) {
+      windowSum -= arr[windowStart];
+      maxSum = Math.max(maxSum, windowSum);
+      windowStart++;
+
     }
   }
-  return largestSoFar;
+  return maxSum;
 };
 
 console.log(max_sub_array_of_size_k(2, [2, 3, 4, 1, 5]));
