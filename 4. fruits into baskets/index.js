6ec1368e748a52cc1412b792581c796758aa67a4
Evan Brecht-Curry <starfoxz13@gmail.com>
2020-02-27 12:17:07 -0800

    finished 5

--- 4. fruits into baskets/index.js
+++ 4. fruits into baskets/index.js
@@ -1,27 +1,27 @@
-const fruits_into_baskets = function(fruits) {
-let windowStart = 0,
-  maxFruit = 0,
-  fruitMap = {};
-
-  for (let windowEnd = 0; windowEnd < fruits.length; windowEnd++) {
-    const rightFruit = fruits[windowEnd];
-    if (!(fruitMap[rightFruit])) {
-      fruitMap[rightFruit] = 0;
-    }
-    fruitMap[rightFruit]++;
-
-    while (Object.keys(fruitMap).length > 2) {
-      const leftFruit = fruits[windowStart];
-      fruitMap[leftFruit] -= 1;
-      if (fruitMap[leftFruit] === 0) {
-        delete fruitMap[leftFruit];
-      }
-      windowStart++;
-    }
-    maxFruit = Math.max(maxFruit, windowEnd - windowStart + 1);
-  }
-  return maxFruit;
-};
-
-console.log('you can fit: ' + fruits_into_baskets(['A', 'B', 'C', 'A', 'C']));
-console.log('you can fit: ' + fruits_into_baskets(['A', 'B', 'C', 'B', 'B', 'C']));
+// const fruits_into_baskets = function(fruits) {
+// let windowStart = 0,
+//   maxFruit = 0,
+//   fruitMap = {};
+//
+//   for (let windowEnd = 0; windowEnd < fruits.length; windowEnd++) {
+//     const rightFruit = fruits[windowEnd];
+//     if (!(fruitMap[rightFruit])) {
+//       fruitMap[rightFruit] = 0;
+//     }
+//     fruitMap[rightFruit]++;
+//
+//     while (Object.keys(fruitMap).length > 2) {
+//       const leftFruit = fruits[windowStart];
+//       fruitMap[leftFruit] -= 1;
+//       if (fruitMap[leftFruit] === 0) {
+//         delete fruitMap[leftFruit];
+//       }
+//       windowStart++;
+//     }
+//     maxFruit = Math.max(maxFruit, windowEnd - windowStart + 1);
+//   }
+//   return maxFruit;
+// };
+//
+// console.log('you can fit: ' + fruits_into_baskets(['A', 'B', 'C', 'A', 'C']));
+// console.log('you can fit: ' + fruits_into_baskets(['A', 'B', 'C', 'B', 'B', 'C']));
--- 5. no repeating substring/index.js
+++ 5. no repeating substring/index.js
@@ -1,16 +1,16 @@
-const fruits_into_baskets = function(fruits) {
+const on_repeat_substring = function(fruits) {
   let windowStart = 0,
     maxFruit = 0,
     fruitMap = {};
 
   for (let windowEnd = 0; windowEnd < fruits.length; windowEnd++) {
     const rightFruit = fruits[windowEnd];
-    if (!(fruitMap[rightFruit])) {
+    if (!(rightFruit in fruitMap)) {
       fruitMap[rightFruit] = 0;
     }
     fruitMap[rightFruit]++;
 
-    while (Object.keys(fruitMap).length > 2) {
+    while (fruitMap[rightFruit] > 1) {
       const leftFruit = fruits[windowStart];
       fruitMap[leftFruit] -= 1;
       if (fruitMap[leftFruit] === 0) {
@@ -23,5 +23,6 @@ const fruits_into_baskets = function(fruits) {
   return maxFruit;
 };
 
-console.log('you can fit: ' + fruits_into_baskets(['A', 'B', 'C', 'A', 'C']));
-console.log('you can fit: ' + fruits_into_baskets(['A', 'B', 'C', 'B', 'B', 'C']));
+console.log('you can fit: ' + on_repeat_substring('aabccbb'));
+console.log('you can fit: ' + on_repeat_substring("abbbb"));
+console.log('you can fit: ' + on_repeat_substring("abccde"));
