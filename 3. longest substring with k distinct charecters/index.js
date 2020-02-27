748632496b1890f5cb7a35560e8e5b41188ffccd
Evan Brecht-Curry <starfoxz13@gmail.com>
2020-02-27 11:14:41 -0800

    finished

--- 3. longest substring with k distinct charecters/index.js
+++ 3. longest substring with k distinct charecters/index.js
@@ -3,26 +3,23 @@ function longest_substring_with_k_distinct(str, k) {
     maxLength = 0,
     charFrequency = {};
 
-  // in the following loop we'll try to extend the range [window_start, window_end]
   for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
     const rightChar = str[windowEnd];
     if (!(rightChar in charFrequency)) {
       charFrequency[rightChar] = 0;
     }
     charFrequency[rightChar] += 1;
-    // shrink the sliding window, until we are left with 'k' distinct characters in the char_frequency
-    while (Object.keys(charFrequency).length > k) {
-      console.log(Object.keys(charFrequency));
+
+    while (Object.keys(charFrequency).length > k ) {
       const leftChar = str[windowStart];
       charFrequency[leftChar] -= 1;
       if (charFrequency[leftChar] === 0) {
         delete charFrequency[leftChar];
       }
-      windowStart += 1; // shrink the window
+      windowStart++;
+      maxLength = Math.max(maxLength, windowEnd - windowStart + 1)
+      }
     }
-    // remember the maximum length so far
-    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
-  }
 
   return maxLength;
 }
